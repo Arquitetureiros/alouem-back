519d2d26c889f44c47c5b3b33be244f63fd79013
gguilioti <giovanni.guilioti@gmail.com>
2023-09-26 20:28:45 -0300

    Publicacao Api

---
+++ app/Models/Publicacao.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Publicacao extends Model
+{
+    use HasFactory;
+
+    protected $table = "Publicacao";
+    protected $primaryKey = "IdPublicacao";
+    public $incrementing = true;
+
+    protected $fillable = [ "Titulo", "descricao", "fk_IdEstado", "VotosPositivos", "VotosNegativos", "fk_IdUsuario"];
+
+    public $timestamps = false;
+}
