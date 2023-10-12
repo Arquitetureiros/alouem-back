63fc661b45cc2231f26d34c3c079a787434f94fe
Leonardo Ribeiro <leonardovribeiro02@gmail.com>
2023-10-12 18:29:10 -0300

    login feature

---
+++ app/Http/Controllers/Auth.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Firebase\JWT\JWT;
+use App\Models\Usuario;
+use App\Models\Moderador;
+
+class Auth extends Controller
+{
+
+    public function login(Request $request){
+
+        $usuario = Usuario::where('Email', $request->email)->where('Senha', $request->senha)->first();
+        $moderador = Moderador::where('Email', $request->email)->where('Senha', $request->senha)->first();
+
+        if($usuario || $moderador){
+            $pessoa = $usuario ? $usuario : $moderador;
+            $tipo = $usuario ? 'usuario' : 'moderador';
+
+            $response = $this->makeJwt($pessoa, $tipo);
+
+            return response()->json(
+                [
+                    "message" => "login realizado com sucesso",
+                    "success" => true,
+                    "jwt" => $response,
+                    'usuario' => $pessoa,
+                ],
+                200
+            );
+
+        } else {
+            return response()->json(
+                [
+                    "message" => "Email ou senha inválidos",
+                    "success" => false
+                ],
+                401
+            );
+        }
+
+
+    }
+
+    private function makeJwt($usuario, $tipo){
+
+        $key = 'alouem';
+
+        $payload = [
+            'id' => isset($usuario->IdUsuario) ? $usuario->IdUsuario : 'admin',
+            'email' => isset($usuario->Email) ? $usuario->Email : $usuario->Email,
+            'tipo' => $tipo
+        ];
+
+        return JWT::encode($payload, $key, 'HS256');
+    }
+}
